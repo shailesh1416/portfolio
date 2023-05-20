ad39b11f472a02065d21269efa5b6af4d334d563
shailesh1416 <shaileshyadav1416@gmail.com>
2023-05-20 21:13:40 +0530

    added color and language feature

--- src/components/poemcard/PoemCard.js
+++ src/components/poemcard/PoemCard.js
@@ -4,32 +4,71 @@ import './PoemCard.css'
 
 function PoemCard({ poem }) {
   const [state, setState] = useState("none");
+  const [language, setLanguage] = useState({
+    previous:null,
+    language:'hindi'
+  });
+  const [color,setColor] = useState({
+    bg:'#000',
+    fg:'#fff'
+  })
 
-  const changeState=()=>{
+  const changeState=(curLang)=>{
     if (state==='none'){
       setState('block')
+      setLanguage({previous:null,language:curLang})
+    }else if(curLang===language.language){
+      setState('none')
+      setLanguage({previous:null,language:'hindi'})
+    }else if(curLang!==language.language){
+      setLanguage({previous:language.language,language:curLang})
     }else{
       setState('none')
     }
   }
 
+  const changeColor=(bg,fg)=>{
+    setColor({
+      bg:bg,
+      fg:fg
+    })
+  }
+
   return (
     <div className='p-2 mx-2 mt-4 text-dark blog-layout bg-light' style={{ border: '1px solid black', borderRadius: '10px' }}>
       <div className='head row'>
-        <div className='col-sm-2 blog-image'>
+        {/* <div className='col-sm-2 blog-image'>
           <img src={`https://drive.google.com/uc?export=view&id=${poem.url}`} style={{width:'100px',borderRadius:'10px'}}/>
+        </div> */}
+        <div className='col-8 d-flex justify-content-left '>
+          <h2 className='mt-3 blog-title px-2'>{poem.title}</h2>
         </div>
-        <div className='col-sm-7 '>
-          <h2 className='mt-3 blog-title text-center'>{poem.title}</h2>
-        </div>
-        <div className='col-sm-3'> 
+        <div className='col-4 d-flex justify-content-end'> 
         {/* https://drive.google.com/file/d/1Wd0QPaemYHV7GTtczYcPuk5wpPZ9BFln/view?usp=share_link */}
-          <button className='p-2 px-4 m-3 rounded-3 bg-primary border text-white' onClick={changeState}>Read</button>
+          <button className='p-2 px-4  rounded-3 bg-primary border text-white' onClick={()=>changeState('hindi')}>
+ह</button>
+          <button className='p-2 px-4  rounded-3 bg-primary border text-white' onClick={()=>changeState('hinglish')}>H</button>
         </div>
-        
       </div>
-      <div dangerouslySetInnerHTML={{ __html: poem.poem }} className='mt-4 text-left' style={{ fontSize: '1.2rem', display: `${ state }` }} />
-      <button className='btn btn-success' onclick={`window.open('whatsapp://send?text=Hey, How are you%0A Read this awesome poem from shailesh ')`}> Share on Mobile </button>
+      <div style={{display: `${ state }`,}}>
+      <div className='d-flex justify-content-evenly'>
+        <button className='colorBtn' style={{backgroundColor:'#FDA492'}} onClick={()=>changeColor('#FDA492','white')}></button>
+        <button className='colorBtn' style={{backgroundColor:'#FED18D'}} onClick={()=>changeColor('#FED18D','black')}></button>
+        <button className='colorBtn' style={{backgroundColor:'#E3D896'}} onClick={()=>changeColor('#E3D896','black')}></button>
+        <button className='colorBtn' style={{backgroundColor:'#51BA69'}} onClick={()=>changeColor('#51BA69','white')}></button>
+        <button className='colorBtn' style={{backgroundColor:'#FFFD55'}} onClick={()=>changeColor('#FFFD55','black')}></button>
+        <button className='colorBtn' style={{backgroundColor:'#AB44FF'}} onClick={()=>changeColor('#AB44FF','white')}></button>
+        <button className='colorBtn' style={{backgroundColor:'#CCBEE2'}} onClick={()=>changeColor('#CCBEE2','black')}></button>
+        <button className='colorBtn' style={{backgroundColor:'#B4A79F'}} onClick={()=>changeColor('#B4A79F')}></button>
+        <button className='colorBtn' style={{backgroundColor:'#1E1E1E',color:'white'}} onClick={()=>changeColor('#1E1E1E','white')}></button>
+      </div>
+      <div>
+      </div>
+      {language.language==='hindi' &&
+      <div dangerouslySetInnerHTML={{ __html: poem.hindi }} className='mt-4 text-left rounded-3 py-5 px-4 poemContent' style={{   backgroundColor:`${color.bg}`,color:`${color.fg}` }} />}
+      {language.language==='hinglish' &&<div dangerouslySetInnerHTML={{ __html: poem.hinglish }} className='mt-4 text-left rounded-3 py-5 px-4' style={{ fontSize: '1.2rem',  backgroundColor:`${color.bg}`,color:`${color.fg}` }} />}
+      </div>
+      {/* <button className='btn btn-success' onclick={`window.open('whatsapp://send?text=Hey, How are you%0A Read this awesome poem from shailesh ')`}> Share on Mobile </button> */}
     </div>
   )
 }
---
+++ src/pages/blogDetail/BlogDetail.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react'
+
+function BlogDetail({hideModal,blogId}) {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+      fetchData()
+    }, [])
+  
+    const fetchData = async () => {
+      const url = `https://portfolio-6viw.onrender.com/blog/${blogId}`
+      try {
+        const response = await fetch(url)
+        const data = await response.json()
+        setData(data) 
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  return (
+    
+    <div class="py-3" id="exampleModal" tabindex="-1" style={{position:'absolute',top:'0',left:'0',height:'100vh',backgroundColor:'white',width:'100%',opacity:'.99',overflow:'auto'}}>
+ 
+      <div class="modal-body" >
+        <h1 className='mt-4 mb-4' >{data.title} </h1>
+      <div class="modal-footer d-flex flex-column justify-content-center p-3" >
+        <img src={`https://drive.google.com/uc?export=view&id=${data.url}`} alt='Blog'className='blogImage'/>
+      <div dangerouslySetInnerHTML={{ __html: data.blog }} className='content mt-4' style={{ fontSize: '1.2rem' }} />
+
+        <button type="button" class="btn btn-danger mt-2" data-dismiss="modal" onClick={hideModal}>Close</button>
+        {/* <button type="button" class="btn btn-primary">Save changes</button> */}
+      </div>
+      </div>    
+  
+    </div>
+   
+  )
+}
+
+export default BlogDetail
